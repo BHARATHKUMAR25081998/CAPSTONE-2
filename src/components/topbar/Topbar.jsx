809e7772372b2ce8560c6cd5996a5b3af0d4e226
BHARATHKUMAR25081998 <87696403+BHARATHKUMAR25081998@users.noreply.github.com>
2021-11-30 18:43:18 +0530

    Update Topbar.jsx

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -33,8 +33,8 @@ export default function Topbar() {
           <Link className="link" to="/settings">
             <img
               className="topImg"
-              src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-              alt=""
+              src=" "
+              alt=" "
             />
           </Link>
         ) : (
